bafed946470076772a076961cd2a7469215db9ec
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2016-02-16 23:05:06 +0000

    Update patchy.php
    Roadmap now downloaded from patchy global server

--- v3.0/assets/php/views/patchy.php
+++ v3.0/assets/php/views/patchy.php
@@ -13,41 +13,21 @@
 					<th>Status</th>
 				</tr>
 			</thead>
-			<tbody>
-				<tr>
-					<td>Admin CP</td>
-					<td>3.x - 4.x</td>
-					<td>Improving</td>
-				</tr>
-				<tr>
-					<td>More archive formats</td>
-					<td>4.x</td>
-					<td>Nope</td>
-				</tr>
-				<tr>
-					<td>More languages</td>
-					<td>4.x</td>
-					<td>Umm..</td>
-				</tr>
-				<tr>
-					<td>File upload progress bar</td>
-					<td>3.x</td>
-					<td>...</td>
-				</tr>
-				<tr>
-					<td>More patch management options</td>
-					<td>4.0</td>
-					<td>Working on</td>
-				</tr>
-				<tr>
-					<td>More AJAX</td>
-					<td>5.0</td>
-					<td>Reviewing..</td>
-				</tr>
+			<tbody id="table_body">
+				
 			</tbody>
 		</table>
 	</div>
 	<div id="patchy">
 		<!--<h2><a href="http://patchy-a.co.nf/" title="Patchy Other">Patchy Global Server</a></h2>-->
+		<script type="text/javascript">
+		$('#table_body').html('<h2>Please wait...</h2>');
+		$(document).ready(function(){
+			var url = 'http://patchy-a.co.nf/update_list.php';
+			$.get(url, function(response) {
+				$('#table_body').html(response);
+			});
+		});
+		</script>
 	</div>
 </div>
